e2c052aeb5f47dde228f492ef614905dc7ebda62
Seevali Rathnayake <devseevali@outlook.com>
2017-06-02 17:29:53 +0530

    Patch usernames with dots (#52)
    Add support for Usernames with `.` (dots)

--- mmail/mattermail_test.go
+++ mmail/mattermail_test.go
@@ -12,7 +12,10 @@ func TestGetChannelFromSubject(t *testing.T) {
 
 	assert("", "")
 	assert("[#test]", "#test")
+	assert("[#test.1]", "#test.1")
 	assert("[@TeSt]", "@test")
+	assert("[@Test.1]", "@test.1")
+	assert("[@TeSt.1]", "@test.1")
 	assert(" [#test]", "#test")
 	assert("   [@test]", "@test")
 	assert("[ #test]", "#test")
